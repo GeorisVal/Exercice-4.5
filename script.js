b85fc32fd5c3c0c521c08cf5235c23a25c2c4ea2
GeorisVal <georis.val@gmail.com>
2022-08-05 12:20:22 +0200

    fixed carousel with Swiper

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'right',
-  contain: true
-});
+function responsive() {
+  if (x.matches) {
+  var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
+  } else {
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
+  }
+};
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
