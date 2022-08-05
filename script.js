9ccefe557a15591f3e5314014dca148c5e85e8ae
GeorisVal <georis.val@gmail.com>
2022-08-05 18:29:17 +0200

    Update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function responsive() {
-  if (x.matches) {
+{
   var swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   slidesPerColumn: 1,
@@ -12,12 +11,5 @@ function responsive() {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-})
-  } else {
-
-  }
-};
-
-var x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+  })
+}
