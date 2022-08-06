6ecdddca940b6b6817ad524eca6d347f55edf3c9
GeorisVal <georis.val@gmail.com>
2022-08-06 12:40:24 +0200

    Added burger menu button animation through JS

--- script.js
+++ script.js
@@ -15,3 +15,22 @@
 }
 
 new cursoreffects.fairyDustCursor({colors: ["#2d1111", "#be5d02", "#964b1b"]});
+
+function checkboxBurger() {
+const checkbox = document.getElementById('checkbox-burger')
+const line1 = document.getElementById('line1')
+const line2 = document.getElementById('line2')
+const line3 = document.getElementById('line3')
+  if (checkbox.checked) {
+    line1.style.transform = "rotate(45deg)"
+    line2.style.transform = "scaleY(0)"
+    line3.style.transform = "rotate(-45deg)"
+    console.log('Checked')
+  }
+  else {
+    line1.style.transform = "rotate(0deg)"
+    line2.style.transform = "scaleY(1)"
+    line3.style.transform = "rotate(0deg)"
+    console.log('Unchecked')
+  }
+}
